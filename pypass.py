8eaf3d5c6882a5fa1a7909506640176845e43358
Colditz Colligula <79027579+ColditzColligula@users.noreply.github.com>
2022-04-18 00:56:18 +0200

    Add files via upload

--- pypass.py
+++ pypass.py
@@ -22,6 +22,7 @@ def generate_password(total_length, upper, lower, digits, symbols, clipb):
         sg.popup(password)
     
 def main():
+    passstrength = "Weak"
     sg.theme('dark grey 5')
 
     layout = [[sg.Text('Uppercase letters'), sg.Push(), sg.Checkbox("", default=True, key="-UPPER-")],
@@ -29,14 +30,28 @@ def main():
                 [sg.Text('Digits'), sg.Push(), sg.Checkbox("", default=True, key="-DIGITS-")],
                 [sg.Text('Symbols'), sg.Push(), sg.Checkbox("", default=True, key="-SYMBOLS-")],
                 [sg.Text('Password Length'), sg.Push(), sg.Slider(range=(4, 128), orientation='h', size=(20, 8), default_value=8, k='-LENGTH-')],
-                [sg.Text('Copy Password to Clipboard?'), sg.Checkbox('', key="-CLIPBOARD-")],
-                    [sg.Button('Generate Password'), sg.Cancel()]]      
+                [sg.Text('Copy Password to Clipboard?'), sg.Push(), sg.Checkbox('', key="-CLIPBOARD-")],
+                [sg.Text('Password Strength: '), sg.InputText(passstrength, key="-PWSTRENGTH-", readonly=False, size=(11,1), background_color="red")],
+                    [sg.Button('Generate Password'), sg.Cancel(), sg.Push(), sg.Button('?')]]      
 
     window = sg.Window('PyPass - Password Generator', layout)    
 
-    event, values = window.read()  
+      
 
     while True:
+        
+        event, values = window.read(timeout=10)
+        if int(values['-LENGTH-']) < 8:
+            window['-PWSTRENGTH-'].update("Very Weak", background_color="purple")
+        if int(values['-LENGTH-']) >= 10:
+            window['-PWSTRENGTH-'].update("Weak", background_color="red")
+        if int(values['-LENGTH-']) >= 12:
+            window['-PWSTRENGTH-'].update("Average", background_color="yellow")
+        if int(values['-LENGTH-']) >= 16:
+            window['-PWSTRENGTH-'].update("Strong", background_color="lime")
+        if int(values['-LENGTH-']) >= 24:
+            window['-PWSTRENGTH-'].update("Very Strong", background_color="green")
+
         if event == "Generate Password":
             if values["-UPPER-"] == True:
                 upper = "1"
@@ -65,6 +80,8 @@ def main():
         if event == sg.WINDOW_CLOSED or event == 'Quit' or event == 'Cancel':
             break
             window.close()
+        if event == '?':
+            sg.Popup("PyPass - A Python3 Password Generator\n\nby Colditz Colligula\n\nVersion 0.2", title='About PyPass')
 
 if __name__ == '__main__':
     main()
